6f726a3fbcd803d6f966f17cf92620df89a299a5
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-02-28 13:53:58 -0800

    Create main.cpp

---
+++ Character Array Reverse/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+//Prototypes
+void reverseFunction();
+
+//Function decleration
+void reverseFunction(char s[])
+{
+  int charLength = strlen(s);  // Getting the length of the character array
+    while(charLength >= 0) // while the length is greater or equal to 0 meaning it will keep decreasing to output the characters in reverse
+    {
+      cout<<s[charLength]; // cout the character starting from position char.length()
+      --charLength; // decrease char length by 1
+    }
+}
+
+int main()
+{
+  reverseFunction("Hello");
+  return 0;
+}
