e34c106117e77210da5150808f65802ce0126b24
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-02-28 18:33:43 -0800

    Update main.cpp

--- Character Array Concatination/main.cpp
+++ Character Array Concatination/main.cpp
@@ -5,27 +5,26 @@
 
 using namespace std;
 
-void concat(const char a[], const char b[],int result_maxlength)
+void concat(const char a[], const char b[], char result[], int result_maxlength)
 {
-  string temp;
-  int aLength = strlen(a);
-  int bLength = strlen(b);
-  for (int i=0; i<aLength; i++)
-    {
-      temp.push_back(a[i]);
-    }
-  for (int i=0; i<bLength; i++)
-    {
-      temp.push_back(b[i]);
-    }
+  strcpy(result, a);
+  strcat(result, b);
   for (int i=0; i<result_maxlength; i++)
     {
-      cout << temp[i];
+      cout << result[i];
     }
 }
 
+
+
 int main()
 {
-  concat("12345", "6789", 8);
+  char a[10] = "12345";
+  char b[10] = "6789";
+  int resultlength = strlen(a) + strlen(b);
+  int result_maxlength = 5;
+  char result[resultlength];
+  
+  concat(a, b, result, result_maxlength);
   return 0;
 }
