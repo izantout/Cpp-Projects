e06bb1cd9ea0bc4c69f9fc00f2829657932f157e
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-02-28 22:08:39 -0800

    Update main.cpp

--- Character Array Concatination/main.cpp
+++ Character Array Concatination/main.cpp
@@ -3,13 +3,17 @@
 
 using namespace std;
 
+// Prototypes
+void concat();
+
+// Function Initialization
 void concat(const char a[], const char b[], char result[], int result_maxlength)
 {
-  strcpy(result, a);
-  strcat(result, b);
-  for (int i=0; i<result_maxlength; i++)
+  strcpy(result, a); // Copy what is in a to result
+  strcat(result, b); // Concatinate what is in b to result
+  for (int i=0; i<result_maxlength; i++) // for loop that goes from 0 to the number the user wants
     {
-      cout << result[i];
+      cout << result[i]; // cout the result untill it reaches where the user specified
     }
 }
 
@@ -17,12 +21,13 @@ void concat(const char a[], const char b[], char result[], int result_maxlength)
 
 int main()
 {
-  char a[] = "12345";
-  char b[] = "6789";
-  int resultlength = strlen(a) + strlen(b);
-  int result_maxlength = 3;
-  char result[resultlength];
-  
+  char a[] = "12345"; // declaring the character array a
+  char b[] = "6789"; // declaring the character array b
+  int resultlength = strlen(a) + strlen(b); // declaring resultlength to be the length of a and the length of b incase the user wants to output both a and b, result should be that big
+  int result_maxlength = 3; // initializing result_maxlength to be whatever we want
+  char result[resultlength]; // initializing the array of characters result to be of size (alength + blength)
+
+  // Function calling
   concat(a, b, result, result_maxlength);
   return 0;
 }
