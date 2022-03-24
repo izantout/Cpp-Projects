9dd319e6f1fe42ad6c578681ca4d6704651f30eb
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-03-24 13:12:22 -0700

    Update employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -1,84 +1,74 @@
 #include "employee.h"
+#include "bankAccount.h"
 #include <iostream>
 #include <string>
 #include <iomanip>
 #include <algorithm>
-#include <vector>
+#include <fstream>
 
 using namespace std;
-vector<Employee> myEmployees;
-vector<string> stringVec;
-Employee::Employee() : Employee("027941626", "Issam", 25.5){
-    
-}
 
-Employee::Employee(string mid, string mname, double msalary){
+Employee::Employee(string mid, string mfirst, string mlast, double msalary, BankAccount* a){
     id = mid;
-    name = mname;
+    first = mfirst;
+    last = mlast;
     salary = msalary;
+    account = a;
 }
 
-void Employee::EmployeeInfo() const{
-  cout << "ID: " << id << endl;
-  cout << "Name: " << name << endl;
-  cout << "Salary: " << salary << endl;
-}
-
-void Employee::print() {
- ifstream infile("salary.txt");
-  ofstream outfile("monthly_salary.txt");
-  string id,first,last, name, menu;
-  string salary;
-  double s, ms, bal;
-  vector<Employee> MyEmployees;
-  int count, width;
-  getline(infile,menu);
-  outfile<<menu<< "     Monthly Salary     " << "   Balance" << endl;
-  while(infile>>id>>last>>first>>salary)
-    {
-      outfile << id       <<           " ";
-      outfile << setw(10) << first  << " ";
-      outfile << setw(11) << last   << " ";
-      name = first + " " +last;
-      salary.erase(remove(salary.begin(), salary.end(), ','), salary.end());
-      s = stod(salary);
-      outfile << setprecision(2) << fixed << setw(7) << "$" << s;
-      ms = s/12;
-      bal = ms+2000;
-      outfile << setprecision(2) << fixed << setw(12-salary.length()) << "$" << ms;
-      if (ms < 999.99)
-      {
-        width = 13;
-      }
-      else if (ms < 9999.99)
-      {
-        width = 12;
-      }
-      else if (ms < 99999.99)
-      {
-        width = 10;
-      }
-      outfile << setprecision(2) << fixed << setw(width) << "$" << bal << endl;
-      stringVec.push_back(id);
-      stringVec.push_back(salary);
-      
-      myEmployees.push_back(Employee(id, name, s));
-      
-    }
-  for (int i=0;i<myEmployees.size();i++)
-    {
-      cout << myEmployees[i];
-    }
+void Employee::print(ofstream outfile) {
+  double monthlySalary = 0;
+  double bal;
+  int width;
+      outfile << id << " ";
+      outfile << setw(10) << first << " ";
+      outfile << setw(11) << last << " ";
+      outfile << setprecision(2) << fixed << setw(7) << salary;
+  monthlySalary = salary/12;
+      outfile << setprecision(2) << fixed << setw(12) << monthlySalary;
+  bal = account->get_balance();
+      outfile << setprecision(2) << fixed << setw(12) << bal << endl;  
 }
 
 ostream& operator<<(ostream& os, const Employee& myEmployee){
-  os << "ID: " << myEmployee.id << endl;
-  os << "Name: " << myEmployee.name << endl;
-  os << "Salary: " << myEmployee.salary << endl;
+  double monthlySalary = 0;
+  int width;
+      os << myEmployee.id << " ";
+      os << setw(10) << myEmployee.getFirst() << " ";
+      os << setw(11) << myEmployee.getLast() << " ";
+      os << setprecision(2) << fixed << setw(7) << "$" << myEmployee.getSalary();
+  monthlySalary = myEmployee.getSalary()/12;
+      os << setprecision(2) << fixed << setw(12) << "$" << monthlySalary;
+      os << setprecision(2) << fixed << setw(12) << "$" <<
+        myEmployee.account->get_balance() << endl;
   return os;
 }
 
+string Employee::getID() {
+  return id;
+}
+
+string Employee::getLast() const{
+  return last;
+}
+
+string Employee::getFirst() const{
+  return first;
+}
+
+double Employee::getSalary() const{
+  return salary;
+}
+
+void Employee::setSalary(double newSalary){
+  salary = newSalary;
+}
+
+void Employee::deposit_monthly_salary(){
+  double sal = getSalary();
+  account->deposit(sal/12);
+}
+
 Employee::~Employee(){
-  cout << "Employee is fired! " << endl;
-  //delete account;
+
 }
