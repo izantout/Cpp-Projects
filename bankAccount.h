e815243e3b444df45958397bac728291ffd14583
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-03-24 13:11:09 -0700

    Create bankAccount.h

---
+++ bankAccount.h
@@ -0,0 +1,39 @@
+#ifndef BANK_ACCOUNT_H
+#define BANK_ACCOUNT_H
+#include <string>
+class BankAccount{
+public:
+   /**
+      Constructs a bank account with a $2000 balance.
+   */
+   BankAccount(std::string id);
+   /**
+      Deposits money into this account.
+      @param amount the amount to deposit.
+   */
+   void deposit(double amount);
+   /**
+      Withdraws money from this account.
+      @param amount the amount to withdraw.
+   */
+   void withdraw(double amount);
+   /**
+      Gets the balance of this account.
+      @return the balance
+   */
+   double get_balance() const;
+   /**
+     Gets the id of this account.
+     @return the balance
+  */
+    std::string get_id() const;
+   /**
+     Set id for bank account.
+     @return the balance
+  */
+    void set_id(std::string id);
+private:
+    std::string id;
+    double balance;
+};
+#endif
