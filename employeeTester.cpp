158fc8d22f8405e172c329d8e2fe04b75c9ad449
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-03-23 21:07:00 -0700

    Create employeeTester.cpp

---
+++ employeeTester.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "employee.h"
+
+using namespace std;
+
+int main()
+{
+  Employee myEmployee;
+  myEmployee.EmployeeInfo();
+  myEmployee.print();
+  return 0;
+}
