24e2a0748dbb3711e6ac797958143745e9a4d86b
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-10 19:53:40 -0700

    Update Shape.cpp

--- Shape Classes & Derived classes/Shape.cpp
+++ Shape Classes & Derived classes/Shape.cpp
@@ -1,1 +1,18 @@
+#include "shape.h"
 
+    /*
+      * Gets the area of the shape
+    */
+double Shape::getArea()
+{
+  return area;
+}
+
+    /*
+      * Sets the area of the shape
+      @ param a for the area
+    */
+void Shape::setArea(double a)
+{
+  area = a;
+}
