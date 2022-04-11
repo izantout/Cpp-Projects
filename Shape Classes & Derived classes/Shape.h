3379dc09bec9ab646a0f82200b5288af18ca9b6c
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-10 19:53:52 -0700

    Update Shape.h

--- Shape Classes & Derived classes/Shape.h
+++ Shape Classes & Derived classes/Shape.h
@@ -1,1 +1,27 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include <iostream>
 
+class Shape{
+
+//the private members included in the class Shape
+  private:
+      double area;
+
+//public member functions of problem 2
+  public:
+    // Getters
+    double getArea();
+
+    // Setters
+    void setArea(double a);
+
+    // virtual calculate area function
+    virtual double calcArea() = 0;
+
+     // Draw function
+    void draw();
+
+};
+
+#endif
