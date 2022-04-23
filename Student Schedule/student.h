ce93853b6cf77d50db7ba89ea539d6b99a9791af
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-22 18:07:40 -0700

    Update student.h

--- Student Schedule/student.h
+++ Student Schedule/student.h
@@ -8,43 +8,95 @@
 
 class Student{
   public:
-    // Default Constructor
+    /*
+      * Default Constructor
+    */
     Student();
 
-    // Overloaded Constructer
+    /*
+      * Overloaded Constructer
+    */
     Student(std::string mstudentID, std::string mfirstName, std::string mlastName, LinkedList<Course>* mmyCourses);
 
-    // Destructor
+    /*
+      * Destructor
+    */
     ~Student();
 
-    // Getters
+    /*
+      * Gets the student ID number
+    */
     std::string getID();
+
+    /*
+      * Gets the students first name
+    */
     std::string getFirst();
+
+    /*
+      * Gets the students last name
+    */
     std::string getLast();
+
+    /*
+      * Gets the students schedule
+    */
     LinkedList<Course>* getSchedule();
 
-    // Setters
+    /*
+      * Sets the student's ID number to a new one
+      @ param string mID for the ID number
+    */
     void setID(std::string mID);
+
+    /*
+      * Sets the student's first name to a new one
+      @ param string mFirst for the first name
+    */
     void setFirst(std::string mFirst);
+
+    /*
+      * Sets the student's last name to a new one
+      @ param string mLast for the last name
+    */
     void setLast(std::string mLast);
+
+    /*
+      * Sets the student's schedule to a new one
+      @ param LinkedList pointer mmyCourse for the new schedule
+    */
     void setSchedule(LinkedList<Course>* mmyCourse);
 
-    // Get initial function
+    /*
+      * Returns the Initials of the student formatted nicely
+    */
     std::string getInitial();
 
-    // Get Full Name Function
+    /*
+      * Returns the full name of the student formatted nicely
+    */
     std::string getFullName();
 
-    // Drop Course
+    /*
+      * Drops a course from the student's schedule
+      @ param myCourse for the course thats going to get dropped
+    */
     void dropCourse(Course myCourse);
 
-    // Add Course
+    /*
+      * Adds a course to the student's schedule
+      @ param myCourse for the course thats going to get added
+    */
     void addCourse(Course myCourse);
 
-    // Sort Courses
+    /*
+      * Sorts the student's courses
+    */
     void sortCourses();
 
-    // toString 
+    /*
+      * returns the schedule of the courses and the student's information nicely
+    */ 
     std::string toString();
 
   private:
