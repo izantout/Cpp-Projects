79cdee85296e95d2dac41a400f1c575672e4225e
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-22 18:08:05 -0700

    Update course.h

--- Student Schedule/course.h
+++ Student Schedule/course.h
@@ -7,28 +7,71 @@
 class Course
   {
     public:
-      // Default Constructor
+    /*
+      * Default Constructor
+    */
       Course();
 
-      // Overloaded Constructor
+    /*
+      * Overloaded Constructor
+    */
       Course(std::string mCNumber, std::string mCName, std::string mCTime, std::string mCSemester);
 
-      // Destructor
+    /*
+      * Destructor
+    */
       ~Course();
 
-      // Getters
+    /*
+      * Gets the course number
+    */
       std::string getCNumber();
+
+    /*
+      * gets the course name
+    */
       std::string getCName();
+
+    /*
+      * gets the course time
+    */
       std::string getCTime();
+
+    /*
+      * gets the course semester
+    */
       std::string getCSemester();
 
-      // Setters
+    /*
+      * Sets the course number to a new one
+      @ param string mCNumber for the new course number
+    */
       void setCNumber(std::string mCNumber);
+
+    /*
+      * Sets the course name to a new one
+      @ param string mCName for the new course name
+    */
       void setCName(std::string mCName);
+
+    /*
+      * Sets the course time to a new one
+      @ param string mdays for the new course days
+      @ paramm sCTime for course start time
+      @ param eCTime for course end time
+    */
       void setCTime(std::string mdays, std::string sCTime, std::string eCTime);
+
+    /*
+      * Sets the course semester to a new one
+      @ param string mCSemester for the new course semester
+      @ param mYear for the new course year
+    */
       void setCSemester(std::string mCSemester, std::string mYear);
 
-      //ToString Function
+    /*
+      * Returns the course details nicely formatted
+    */
       std::string toString();
 
     private:
