36a06248544951dbd696c7d752f0144853a6a36a
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-22 18:12:30 -0700

    Update student.cpp

--- Student Schedule/student.cpp
+++ Student Schedule/student.cpp
@@ -59,13 +59,13 @@ void Student::setSchedule(LinkedList<Course>* mmyCourse){
   }
   ListNode<Course>* currentNode = mmyCourse->head; 
   while(currentNode != nullptr){// while nott the end of the linked list
-    myCourses->insertNode(currentNode->value);
-    currentNode = currentNode->next; //
+    myCourses->insertNode(currentNode->value); // add the nodes back
+    currentNode = currentNode->next; //update node and next node
   }
 }
 
 // Get initial function
-string Student::getInitial(){
+string Student::getInitial(){ // adds first name initial and last name initial with 2 points to final string
   string finalString = "";
   finalString += firstName[0]; 
   finalString += ".";
@@ -75,34 +75,34 @@ string Student::getInitial(){
 }
 
 // Get Full Name Function
-string Student::getFullName(){
+string Student::getFullName(){ // last name , first name
   return lastName + ", " + firstName;
 }
 
 // Drop Course
-void Student::dropCourse(Course myCourse){
+void Student::dropCourse(Course myCourse){ //  drops the course by deleting the node
   myCourses->deleteNode(myCourse);
 }
 
 // Add Course
-void Student::addCourse(Course myCourse){
+void Student::addCourse(Course myCourse){ // adds the course to the end of the linked list
   myCourses->appendNode(myCourse);
 }
 
 void Student::sortCourses(){ 
   LinkedList<Course> sortedCourses;
   ListNode<Course>* currentNode;
-  if(myCourses->head){
+  if(myCourses->head){ // adds courses to sortedCourses
     currentNode = myCourses->head;
     while(currentNode){
-      sortedCourses.insertNode(currentNode->value);
+      sortedCourses.insertNode(currentNode->value);// adds them by inserting them which is sorting them and adding them
       currentNode = currentNode->next;
     }
-  while(myCourses->head){
+  while(myCourses->head){ // delet the nodes from myCourses for it to be empty
     myCourses->deleteNode((myCourses->head)->value);
   }
   ListNode<Course>* currentNode = sortedCourses.head; 
-  while(currentNode != nullptr){
+  while(currentNode != nullptr){// add the sorted list back to myCourses and returns it
     myCourses->insertNode(currentNode->value);
     currentNode = currentNode->next;
   }
@@ -110,7 +110,7 @@ void Student::sortCourses(){
 }
 
 // toString 
-string Student::toString(){
+string Student::toString(){// returns the info in a nice format
   return "ID: " + studentID +"\n" + "First Name: " + firstName +"\n" + "Last Name: " + lastName + "\n" + "Schedule for " + myCourses->head->value.getCSemester() + "\n" + myCourses->toString();
     
 }
