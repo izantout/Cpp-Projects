88a90831f41bd9960b8945700727e8d348455f30
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-28 16:17:10 -0700

    Create Card.cpp

---
+++ Receipt Project/Card.cpp
@@ -0,0 +1,54 @@
+#include "Card.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Default Constructor
+Card::Card(){
+  
+}
+
+// Overloaded Contrustor
+Card::Card(std::string mNumber, std::string mPin){
+  cardNumber = mNumber;
+  cardPin = mPin;
+}
+
+//Destructor
+Card::~Card(){
+  
+}
+
+// Getters
+string Card::getNumber(){
+  return cardNumber;
+}
+
+string Card::getPin(){
+  return cardPin;
+}
+
+// Setters
+void Card::setNumber(string mNumber){
+  cardNumber = mNumber;
+}
+
+void Card::setPin(string mPin){
+  cardPin = mPin;
+}
+
+// toString function
+string Card::toString(){
+  int numberLength = cardNumber.length();
+  string coveredNumber = "";
+  for (int i=0; i<numberLength-4; i++)
+    {
+      coveredNumber += "*";
+    }
+  for (int i=(numberLength-4); i<numberLength; i++)
+    {
+      coveredNumber+= cardNumber[i];
+    }
+  return coveredNumber;
+}
