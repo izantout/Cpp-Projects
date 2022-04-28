ee55a5bea09aa7b35034f48616775586bdfb0e69
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-28 16:18:06 -0700

    Create Products.cpp

---
+++ Receipt Project/Products.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include "Products.h"
+
+using namespace std;
+
+// Defualt Constructor
+Products::Products(){
+  
+}
+
+// Overloaded Constructor
+Products::Products(string mName, string SNumber, int mQuantity, double mPrice){
+  name = mName;
+  SerialNumber = SNumber;
+  quantity = mQuantity;
+  price = mPrice;
+}
+
+// Getters
+string Products::getName(){
+  return name;
+}
+
+string Products::getSNumber(){
+  return SerialNumber;
+}
+
+int Products::getQuantity(){
+  return quantity;
+}
+
+double Products::getPrice(){
+  return price;
+}
+
+// Setters
+void Products::setName(string mName){
+  name = mName;
+}
+
+void Products::setSNumber(string mNumber){
+  SerialNumber = mNumber;
+}
+
+void Products::setQuantity(int mQuantity){
+  quantity = mQuantity;
+}
+
+void Products::setPrice(double mPrice){
+  price = mPrice;
+}
+
+// ToString
+string Products::toString(){
+  return "Product: " + name + "\n" + 
+         "Serial Number: " + SerialNumber + "\n" + 
+         "Quantity: " + to_string(quantity) + "\n" + 
+         "Price: " +  to_string(price);
+}
