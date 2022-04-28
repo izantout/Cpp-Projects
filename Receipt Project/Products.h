a588829dad750e0cbf39adce909818caf5537651
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-28 16:18:19 -0700

    Create Products.h

---
+++ Receipt Project/Products.h
@@ -0,0 +1,35 @@
+#ifndef PRODUCTS_H
+#define PRODUCTS_H
+#include <string>
+
+class Products{
+  public:
+    // Defualt Constructor
+    Products();
+
+    // Overloaded Constructor
+    Products(std::string mName, std::string SNumber, int mQuantity, double mPrice);
+    
+    // Getters
+    std::string getName();
+    std::string getSNumber();
+    int getQuantity();
+    double getPrice();
+    
+    // Setters
+    void setName(std::string mName);
+    void setSNumber(std::string mNumber);
+    void setQuantity(int mQuantity);
+    void setPrice(double mPrice);
+
+    // ToString
+    std::string toString();
+
+  private:
+    std::string name;
+    std::string SerialNumber;
+    int quantity;
+    double price;
+};
+
+#endif
