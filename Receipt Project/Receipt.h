a60c1863740aa8c11a0a511557e98dea41e85847
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-28 18:54:16 -0700

    Update Receipt.h

--- Receipt Project/Receipt.h
+++ Receipt Project/Receipt.h
@@ -8,27 +8,28 @@
 class Receipt{
   public:
     Receipt();
-    Receipt(std::string mheader, std::string mbuySummary, std::string mbarcode, std::string mfooter);
+    Receipt(std::string mheader, std::vector<Products> mbuySummary, std::string mbarcode, std::string mfooter);
     ~Receipt();
 
     // Getters
     std::string getHeader();
-    std::string getSummary();
+    std::vector<Products> getSummary();
     std::string getBarcode();
     std::string getFooter();
     
     // Setters
     void setHeader(std::string mheader);
-    void setSummary(std::vector<Products> myProducts);
+    void setSummary(std::vector<Products> mmyProducts);
     void setBarcode(std::string mBarcode);
     void setFooter(std::string mFooter);
 
     //toString Function
     std::string toString();
+    std::string trial();
 
   private:
     std::string header;
-    std::string buySummary;
+    std::vector<Products> buySummary;
     std::string barcode;
     std::string footer;
 };
