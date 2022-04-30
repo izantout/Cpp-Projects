0e27f66d24459a6a09cbbf161b99047220c67cf9
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-04-30 16:39:09 -0700

    Create Barcode.h

---
+++ Receipt Project/Barcode.h
@@ -0,0 +1,43 @@
+#ifndef BARCODE_H
+#define BARCODE_H
+#include <iostream>
+#include <string>
+
+class Barcode{
+  public:
+    // Default constructor
+    Barcode();
+    
+    //Overloaded Constructor
+    Barcode(std::string mdate, std::string mtime, std::string memployeeName, std::string mcashierNumber);
+    
+    // Desturtcor
+    ~Barcode();
+
+    // Getters
+    std::string getBarcode();
+    std::string getDate();
+    std::string getTime();
+    std::string getName();
+    std::string getNumber();
+    
+    // Setters
+    void setBarcode();
+    void setDate(std::string mdate);
+    void setTime(std::string mtime);
+    void setName(std::string mname);
+    void setNumber(std::string mnumber);
+
+    // toString function
+    std::string toString();
+
+  private:
+    std::string barcode;
+    std::string date;
+    std::string ptime;
+    std::string employeeName;
+    std::string cashierNumber;
+};
+
+
+#endif 
