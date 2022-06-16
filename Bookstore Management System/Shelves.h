5485a3ea9394d92ed44273066eaeb2431a2be07d
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-06-16 04:59:16 -0700

    Create Shelves.h

---
+++ Bookstore Management System/Shelves.h
@@ -0,0 +1,25 @@
+#ifndef SHELVES_H
+#define SHELVES_H
+
+#include <string>
+#include <vector>
+#include "Books.h"
+
+class Shelves{
+  private:
+    std::vector<Books> myBooks;
+    std::vector<std::string> myBookNames;
+  public:
+    Shelves();
+    Shelves(std::vector<Books> myBooks);
+    ~Shelves();
+    void getBooks();
+    void setBooks(std::vector<Books> mmyBooks);
+    void orderBooks(std::vector <Books> myBooks);
+    void searchfor();
+
+    friend bool operator==(const std::string& left, const std::string& right);
+
+};
+
+#endif
