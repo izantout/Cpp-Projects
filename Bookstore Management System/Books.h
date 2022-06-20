6c9193838415d7c46d7dcc622642be3f42d9ae09
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-06-20 07:00:39 -0700

    Update Books.h

--- Bookstore Management System/Books.h
+++ Bookstore Management System/Books.h
@@ -12,12 +12,15 @@ class Books{
     double Price;
     std::string Content;
     int inventory;
+    std::string bookNumber;
 
   public:
 
     Books();
     Books(std::string mName);
-    Books(std::string mName, std::string mAuthorName, std::string mAuthorLast, int mPages, double mPrice, std::string mContent, int minventory);
+    Books(std::string mName, std::string mbookNumber);
+    Books(std::string mName, std::string mAuthorName, std::string mAuthorLast, int mPages, double mPrice, int minventory, std::string mbookNumber);
+    Books(std::string mName, std::string mAuthorName, std::string mAuthorLast, int mPages, double mPrice, std::string mContent, int minventory, std::string mbookNumber);
     ~Books();
 
     std::string getName();
@@ -25,14 +28,16 @@ class Books{
     int getPages();
     double getPrice();
     std::string getContent();
+    std::string getNumber();
+    int getInventory();
 
     void setName(std::string mname);
-    void setAuthor(std::string mAuthorfirst, std::string mAutherLast);
+    void setAuthor(std::string mAuthorfirst,            std::string mAutherLast);
     void setPages(int mpages);
     void setPrice(double mprice);
     void setContent(std::string mContent);
+    void setInventory(int mInventory);
 
-    friend Books operator++(Books myBooks);
 };
 
 
