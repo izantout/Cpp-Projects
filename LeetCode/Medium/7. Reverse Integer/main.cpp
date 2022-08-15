96c481e56b87a2ebb017575d030b657268aeeb0f
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-08-14 21:42:28 -0700

    Create main.cpp

---
+++ LeetCode/Medium/7. Reverse Integer/main.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverse(int x) { 
+        long res = 0;
+        while (x != 0) {
+            res = 10 * res + x % 10;
+            x /= 10;
+        }
+        return (res > INT_MAX || res < INT_MIN) ? 0 : res;
+    }
+};
