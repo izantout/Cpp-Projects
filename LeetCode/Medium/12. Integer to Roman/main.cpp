b5668d746d620aa008f9cf4c8f367f37f5b7b08d
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-08-14 21:40:03 -0700

    Update main.cpp

--- LeetCode/Medium/12. Integer to Roman/main.cpp
+++ LeetCode/Medium/12. Integer to Roman/main.cpp
@@ -11,3 +11,8 @@ public:
     }
 };
 
+/*
+- for loop starts with i=0, comparing N with 1000 if N < 1000 it goes to 900.... until N becomes >= to val[i]. 
+- It adds rom[i] to the string and removes val[i] from N and keeps doing it untill N becomes less than val[i]
+- it increments i meaning the number N is compared to becomes smaller
+- does the same thing, takes the rom[i] and subtracts N until N becomes 0 and you return the string
