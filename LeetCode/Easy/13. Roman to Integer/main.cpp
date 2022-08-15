4e52687ffb5803a7dd57b56cc3c279b59560a3e0
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-08-14 20:48:30 -0700

    Update main.cpp

--- LeetCode/Easy/13. Roman to Integer/main.cpp
+++ LeetCode/Easy/13. Roman to Integer/main.cpp
@@ -22,3 +22,27 @@ public:
         return final;
     }
 };
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> roman ={{ 'I' , 1 },
+                              { 'V' , 5 },
+                              { 'X' , 10 },
+                              { 'L' , 50 },
+                              { 'C' , 100 },
+                              { 'D' , 500 },
+                              { 'M' , 1000 }};
+
+        int final = 0, strLength = s.length();
+        for(int i=0; i<strLength; i++){
+            if(i == (strLength-1) || (roman[s[i]]>= roman[s[i+1]])){
+                final += roman[s[i]];
+            } else {
+                final += (roman[s[i+1]] - roman[s[i]]);
+                i++;
+            }
+        }
+        return final;
+    }
+};
