8c5660e0cbe9f006deca4b93a8bf2bc047c88f8f
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-08-25 17:49:36 -0700

    Update main.cpp

--- LeetCode/Easy/20. Valid Parentheses/main.cpp
+++ LeetCode/Easy/20. Valid Parentheses/main.cpp
@@ -32,3 +32,57 @@ int main(){
   cout << "flag is " << flag << endl;
   return flag;
 }
+
+
+// stack solution found online
+class Solution {
+    vector<char> my_stack;
+public:
+    bool isValid(string s) {
+		if (s.size() % 2 != 0)
+            return false;
+        if (s.size() == 0)
+            return true;
+        for (int i = 0; i < s.size(); i++)
+        {
+                my_stack.push_back(s[i]);
+				// checking if char pushed on stack is closing brace
+                if (my_stack[my_stack.size() - 1] == '}')
+                {
+				    // if it's a closing brace and is not pushed on empty stack
+                    if (my_stack.size() > 1){
+					// check for pairs
+                    if(my_stack[my_stack.size() - 2] == '{')
+                    {
+					   // pop if pairs formed
+                        my_stack.pop_back();
+                        my_stack.pop_back();
+                    }
+                    }
+                }
+                else if (my_stack[my_stack.size() - 1] == ')')
+                {
+                    if (my_stack.size() > 1){
+                    if(my_stack[my_stack.size() - 2] == '(')
+                    {
+                        my_stack.pop_back();
+                        my_stack.pop_back();
+                    }
+                    }
+                }
+                else if (my_stack[my_stack.size() - 1] == ']')
+                {
+                    if (my_stack.size() > 1){
+                    if(my_stack[my_stack.size() - 2] == '[')
+                    {
+                        my_stack.pop_back();
+                        my_stack.pop_back();
+                    }
+                    }
+                }  
+        }
+        if (!my_stack.empty())
+            return false;
+        return true;
+    }
+};
