ca7124f4e8382d8b5f625b0840c313ed499755b0
Issam Zantout <80976696+izantout@users.noreply.github.com>
2022-08-25 17:39:05 -0700

    Update main.cpp

--- LeetCode/Easy/1. Two Sum/main.cpp
+++ LeetCode/Easy/1. Two Sum/main.cpp
@@ -16,3 +16,26 @@ public:
         return solution;
     }
 };
+
+// faster solution
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       
+        //We will store our values and the indices here.
+        unordered_map<int,int> map;        
+		
+        //We iterate through the loop.
+        for(int index = 0; index < nums.size(); index++){
+			if(map.find(target-nums[index]) != map.end()){
+                //If map contains the value target - nums[index] return the indices of these two values.
+                return {index, map[target-nums[index]]};
+            }			
+            map[nums[index]] = index;
+            //If not add nums[index] to map.
+        }
+        
+        //If there is no solution return an empty vector.        
+        return {};
+    }   
+};
