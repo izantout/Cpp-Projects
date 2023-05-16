7c7620de6f6979e0b46c9c0439e13673d069ee85
Issam Zantout <80976696+izantout@users.noreply.github.com>
2023-05-15 18:25:56 -0700

    Create main.cpp

---
+++ LeetCode/24. Swap Nodes In Pairs/main.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* current = head;
+        int i = 0;
+        while(current != nullptr){
+            i++;
+            current = current->next;
+        }
+        cout << "Length is: " << i << endl;
+        current = head;
+        while(current != nullptr){
+            cout << "I am at: " << current->val << endl;
+            if(current->next != nullptr){
+                int temp = current->val;
+                cout << "Current Val is: " << current->val << endl;
+                current->val = current->next->val;
+                current->next->val = temp;
+                cout << "After change, current val is: " << current->val << endl;
+                cout << "After change, next val is: " << current->next->val << endl;
+                current = current->next->next;
+            } else {
+                current = current->next;
+            }
+        }
+        return head;
+    }
+};
